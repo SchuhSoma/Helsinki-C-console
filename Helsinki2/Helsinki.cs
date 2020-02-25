fc9e947c143b570828ea089d3b89757e5438e63e
SchuhSoma <60043511+SchuhSoma@users.noreply.github.com>
2020-02-25 21:02:57 +0100

    Add files via upload

---
+++ Helsinki2/Helsinki.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helsinki2
+{
+    class Helsinki
+    {
+        public int Helyezes;
+        public int SportolokSzama;
+        public string SportagNeve;
+        public string VersenyszamNeve;
+
+        public Helsinki( string sor)
+        {
+            var dbok = sor.Split(' ');
+            this.Helyezes = int.Parse(dbok[0]);
+            this.SportolokSzama = int.Parse(dbok[1]);
+            this.SportagNeve = dbok[2];
+            this.VersenyszamNeve = dbok[3];
+        }
+
+
+
+    }
+}
